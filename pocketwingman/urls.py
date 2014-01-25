d1899b1a035b617ff514d0ddfb11251fc8c4c6ef
Greg <greg15001@gmail.com>
2014-01-25 12:49:37 -0500

    added an attraction view and template

--- pocketwingman/urls.py
+++ pocketwingman/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, url
 from pocketwingman import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^/helpme$', views.HelpMeView.as_view(), name='helpme'),
-    url(r'^/helpout$', views.HelpOutView.as_view(), name='helpout'),
-    url(r'^/helpme/results/$', views.ResultsView.as_view(), name='help_me_results'),
-    url(r'^/helpme/results/$', views.ResultsView.as_view(), name='help_out_results')
+    url(r'^$', views.index, name='index'),
+    url(r'^help_me_all_purpose$', views.help_me_all_purpose, name='help_me_all_purpose')
+#    url(r'^$', views.IndexView.as_view(), name='index')
+#    url(r'^/helpme$', views.HelpMeView.as_view(), name='helpme'),
+#    url(r'^/helpout$', views.HelpOutView.as_view(), name='helpout'),
+#    url(r'^/helpme/results/$', views.ResultsView.as_view(), name='help_me_results'),
+#    url(r'^/helpme/results/$', views.ResultsView.as_view(), name='help_out_results')
 
 )
--- pocketwingman/views.py
+++ pocketwingman/views.py
@@ -6,13 +6,30 @@ from django.utils import timezone
 
 from pocketwingman.models import Category, Result
 
-class IndexView(generic.ListView):
-    template_name = 'pocketwingman/index.html'
-    context_object_name = 'latest_result_list'
+#class IndexView(generic.ListView):
+#    template_name = 'pocketwingman/index.html'
+#    context_object_name = 'latest_result_list'
 
-    def get_queryset(self):
-            """
-            Return the last five categories
-            """
-            #return Category.objects.filter().order_by('-created_on')[:5]
-            return Result.objects.filter()
+#    def get_queryset(self):
+#            """
+#            Return the last five categories
+#            """
+#            #return Category.objects.filter().order_by('-created_on')[:5]
+#            return Result.objects.filter()
+
+def index(request):
+    latest_result_list = Result.objects.all().order_by('created_on')[:1]
+    context = {'latest_result_list': latest_result_list}
+    return render(request, 'pocketwingman/index.html', context)
+
+def index(request):
+    latest_result_list = Result.objects.all().order_by('created_on')[:1]
+    context = {'latest_result_list': latest_result_list}
+    return render(request, 'pocketwingman/index.html', context)
+
+
+def help_me_all_purpose(request):
+    latest_result_list = Result.objects.filter(category_id__in=[5])
+    context = {'latest_result_list': latest_result_list}
+    return render(request, 'pocketwingman/index.html', context)
+
