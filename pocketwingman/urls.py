fab66debf7876956c8fc22296f1b698eb9fbd9ce
Greg <greg15001@gmail.com>
2014-01-26 16:12:15 -0500

    Added 2 help_me and help_out pages

--- pocketwingman/urls.py
+++ pocketwingman/urls.py
@@ -4,7 +4,9 @@ from pocketwingman import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<category_id>\d+)/$', views.help_me, name='help_me'),
-    url(r'^help_out/(?P<category_id>\d+)/$', views.help_out, name='help_out'),
+    url(r'^help_me/$', views.help_me, name='help_me'),
+    url(r'^help_out/$', views.help_out, name='help_out'),
+    url(r'^help_me/(?P<category_id>\d+)/$', views.help_me_result, name='help_me_result'),
+    url(r'^help_out/(?P<category_id>\d+)/$', views.help_out_result, name='help_out_result'),
 
 )
--- pocketwingman/views.py
+++ pocketwingman/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.http import request
+from django.http import request, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -22,17 +22,25 @@ from pocketwingman.forms import ResultForm, CategoryForm
 #            return Result.objects.filter()
 
 def index(request):
-    latest_result_list = Result.objects.all().order_by('created_on')[:1]
-    context = {'latest_result_list': latest_result_list}
-    return render(request, 'pocketwingman/index.html', context)
+    return render(request, 'pocketwingman/index.html')
 
-
-def help_me(request,category_id):
-    latest_result_list = Result.objects.filter(category_id__in=[category_id])
-    context = {'latest_result_list': latest_result_list}
+def help_me(request):
+    latest_category_list = Category.objects.all().order_by('id')
+    context = {'latest_category_list': latest_category_list}
     return render(request, 'pocketwingman/help_me.html', context)
 
-def help_out(request, category_id):
+def help_out(request):
+    latest_category_list = Category.objects.all().order_by('id')
+    context = {'latest_category_list': latest_category_list}
+    return render(request, 'pocketwingman/help_out.html', context)
+
+
+def help_me_result(request,category_id):
+    latest_result_list = Result.objects.filter(category_id__in=[category_id])
+    context = {'latest_result_list': latest_result_list}
+    return render(request, 'pocketwingman/help_me_result.html', context)
+
+def help_out_result(request, category_id):
     if request.method == 'POST':
         form = ResultForm(request.POST)
         if form.is_valid():
@@ -50,5 +58,5 @@ def help_out(request, category_id):
     else:
         form = ResultForm()
     context = {'form': form, 'category_id': category_id}
-    return render(request,'pocketwingman/help_out.html', context)
+    return render(request,'pocketwingman/help_out_result.html', context)
 
