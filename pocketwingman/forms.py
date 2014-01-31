2c9f2de730fd8c4c77cc7c486cbfe6edad161dd9
Greg <greg15001@gmail.com>
2014-01-31 14:55:50 -0500

    Added new form for post on help_me

--- pocketwingman/forms.py
+++ pocketwingman/forms.py
@@ -10,11 +10,24 @@ class CategoryForm(forms.Form):
 
 
 
-class ResultForm(forms.ModelForm):
+class ResultFormHelpOut(forms.ModelForm):
     category_result = forms.CharField(max_length=200,help_text="Your best line")
     rating = forms.DecimalField(widget=forms.HiddenInput(), initial=0)
-    votes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    ratings_count = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    votes = forms.IntegerField(widget=forms.HiddenInput(), initial=1)
+    ratings_count = forms.IntegerField(widget=forms.HiddenInput(),initial=1)
+    created_by = forms.IntegerField(widget=forms.HiddenInput(), initial=1)
+
+    class Meta:
+        model = Result
+        exclude = ('category', 'ratings_count')
+        #fields = ('category_result', 'rating', 'votes','category')
+
+
+class ResultFormHelpMe(forms.ModelForm):
+    category_result = forms.CharField(max_length=200,help_text="Your best line")
+    rating = forms.DecimalField(widget=forms.HiddenInput(), initial=0)
+    votes = forms.IntegerField(widget=forms.HiddenInput(), initial=1)
+    #ratings_count = forms.IntegerField(widget=forms.HiddenInput(),initial=1)
     created_by = forms.IntegerField(widget=forms.HiddenInput(), initial=1)
 
     class Meta:
--- pocketwingman/models.py
+++ pocketwingman/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+
 class Category(models.Model):
     type_of_category = models.CharField(max_length=200)
     created_on = models.DateTimeField('created on')
@@ -10,12 +11,11 @@ class Category(models.Model):
 class Result(models.Model):
     category = models.ForeignKey(Category)
     category_result = models.CharField(max_length=200)
-    rating = models.DecimalField('', '', 8, 3)
-    ratings_count = models.IntegerField(default=0)
-    votes = models.IntegerField(default=0)
+    rating = models.DecimalField('', '', 8, 3,null=True)
+    ratings_count = models.IntegerField(default=0,null=True)
+    votes = models.IntegerField(default=1,null=True)
     created_by = models.IntegerField(default=0, null=True, blank=True)
     created_on = models.DateTimeField(auto_now=True)
     def __unicode__(self):
         return self.category_result
-
 
