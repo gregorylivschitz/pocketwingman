9d4d4fe18dfd32f9507900a1a8b45a6b93176f1c
Gregory Livschitz <greg15001@gmail.com>
2014-02-15 22:16:25 -0500

    Added ajax call for hardmode switch and changed up the ui button

--- pocketwingman/static/js/main.js
+++ pocketwingman/static/js/main.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
-    $("#enable-hard-mode").click(function(){
-        $.get("/pocketwingman/hard_mode");
-    });
-
-     $("#enable-easy-mode").click(function(){
-        $.get("/pocketwingman/easy_mode");
-    });
-
     $("[name='my-checkbox']").bootstrapSwitch();
+
+    $('#enable-easy-mode-switch').on('switchChange', function (e, data) {
+        var $element = $(data.el),
+        value = data.value;
+        console.log(e, $element, value);
+        if (value) {
+            console.log("In hard mode");
+            $.get("/pocketwingman/hard_mode");
+        }
+        else {
+            console.log("In easy mode");
+            $.get("/pocketwingman/easy_mode");
+        }
+
+    });
 });
