46f57856ce03cf3d8558e13d57ea9f8ab76a62f7
Greg <greg15001@gmail.com>
2014-02-26 23:32:34 -0500

    merging

--- pocketwingman/static/js/main.js
+++ pocketwingman/static/js/main.js
@@ -21,8 +21,12 @@ $(document).ready(function(){
         $("#id_votes").val("1");
         $.post($("#result_form").attr('action'), $('#result_form').serialize());
 
+        .done(function() {
+            alert( "second success" );
+        })
+
         $.get("/pocketwingman/help_me/ajax/" + category_id, function(data){
-            $("#user_name").html("<i>Submitted by "+data.user_name+"</i>");
+            $("#user_name").html("<i>Submitted "+data.user_name+"</i>");
             $("#result_category_result").html(data.category_result);
             $("#result_votes").html(data.result_vote);
 
